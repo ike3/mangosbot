4cd47c40b1747be284e0c02ab5280e0f9f5c3bd1
tkrokli <tkrokli@hotmail.com>
2015-05-04 05:13:15 +0200

    Authserver + Worldserver config file/folder change
    More documentation on the changes from Feyzee

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -51,7 +51,7 @@
 using namespace boost::program_options;
 
 #ifndef _TRINITY_CORE_CONFIG
-    #define _TRINITY_CORE_CONFIG  "worldserver.conf"
+    #define _TRINITY_CORE_CONFIG  "Settings/worldserver.conf"
 #endif
 
 #define WORLD_SLEEP_CONST 50
